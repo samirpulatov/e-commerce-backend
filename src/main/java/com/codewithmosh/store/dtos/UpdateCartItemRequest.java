a19b0e206caf282f01077ad18d9376e704c27280
Samir <samirpulatov42@gmail.com>
2025-08-22 22:06:17 +0500

    feat(cart): add endpoint to remove a product from the cart

--- src/main/java/com/codewithmosh/store/dtos/UpdateCartItemRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/UpdateCartItemRequest.java
@@ -1,10 +1,16 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateCartItemRequest {
+    @NotNull(message = "Quantity must be provided")
+    @Min(value = 1, message = "Quantity must be greater than zero")
+    @Max(value = 100,message = "Quantity must be less than or equal to 100.")
     public Integer quantity;
 }
--- src/main/java/com/codewithmosh/store/entities/Cart.java
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -25,7 +25,7 @@ public class Cart {
     private LocalDate dateCreated;
 
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE,fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE,fetch = FetchType.EAGER,orphanRemoval = true)
     private Set<CartItem> cartItems = new LinkedHashSet<>();
 
     public BigDecimal getTotalPrice() {
@@ -34,5 +34,36 @@ public class Cart {
             total = total.add(cartItem.getTotalPrice());
         }
         return total;
+    }
+
+    public CartItem getItem(Long productId) {
+        return cartItems.stream().filter(cartItem -> cartItem.getProduct().getId().equals(productId)).findFirst().orElse(null);
+    }
+
+    public CartItem addItem(Product product) {
+        var cartItem = getItem(product.getId());
+        if (cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity()+1);
+        }else {
+            cartItem = new CartItem();
+            cartItem.setQuantity(1);
+            cartItem.setProduct(product);
+            cartItem.setCart(this);
+            cartItems.add(cartItem);
+        }
+
+        return cartItem;
+    }
+
+    public void removeItem(Long productId) {
+        var cartItem = getItem(productId);
+        if (cartItem != null) {
+            cartItems.remove(cartItem);
+            cartItem.setQuantity(null);
+        }
+    }
+
+    public void clear() {
+        cartItems.clear();
     }
 }
