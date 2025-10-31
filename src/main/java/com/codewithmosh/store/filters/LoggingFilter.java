00b90f34b91f40d6cefa699b498b88499879f479
Samir Pulatov <samirpulatov42@gmail.com>
2025-10-31 14:50:48 +0100

    feat(auth): add JwtService for token generation

---
+++ src/main/java/com/codewithmosh/store/filters/LoggingFilter.java
@@ -0,0 +1,4 @@
+package com.codewithmosh.store.filters;
+
+public class LoggingFilter {
+}
--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -1,13 +1,20 @@
 package com.codewithmosh.store.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400; // 1 day in seconds
 
@@ -16,8 +23,37 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
+
+
+    public boolean validateToken(String token) {
+        try {
+
+            var claims = getClaims(token);
+            // Return true if token is not expired
+            return claims.getExpiration().after(new Date());
+        } catch (JwtException e) {
+            // Return false if token is invalid or expired
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        // Parse and verify JWT signature using the secret key
+        // extract claims
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes())) // verify signature
+                .build()
+                .parseSignedClaims(token) // parse and validate token
+                .getPayload();
+    }
+
+
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
+    }
+
 }
 
