711a384a73b9b92bef077afc19019c421698b19d
Samir Pulatov <samirpulatov42@gmail.com>
2025-10-31 14:52:29 +0100

    chore(logging): add LoggingFilter for request/response logging

--- src/main/java/com/codewithmosh/store/filters/LoggingFilter.java
+++ src/main/java/com/codewithmosh/store/filters/LoggingFilter.java
@@ -1,4 +1,28 @@
 package com.codewithmosh.store.filters;
 
-public class LoggingFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Log incoming request URI
+        System.out.println("Request URI: " + request.getRequestURI());
+
+        // Continue with the next filter or controller
+        filterChain.doFilter(request, response);
+
+        // Log response status after processing
+        System.out.println("Response: " + response.getStatus());
+    }
 }
+
